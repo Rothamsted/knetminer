3b5844ad4152270293f3cfc48ac94db422b2bda8
mcastellote <castellotemartin@yahoo.com.ar>
2013-10-10 15:29:51 +0100

    Update feature: Organisms without a reference genome - Client and Server Side Description: example of configuration in tomato

--- tomato/client/src/main/webapp/html/javascript/utils-config.js
+++ tomato/client/src/main/webapp/html/javascript/utils-config.js
@@ -2,4 +2,5 @@ var data_url = "http://ondex.rothamsted.ac.uk/tomato_data/annotation/generated/"
 var graph_url = "http://ondex.rothamsted.ac.uk/tomato_data/graph/generated/";
 var applet_url = "http://ondex.rothamsted.ac.uk/applet-0.5.0/";
 var species = 'Tomato Genome';
-var reference_genome = false;
+var reference_genome = false; //true if you are providing a reference genome
+var multiorganisms = true; //true if you specified more than one taxid in the server file config.xml (ie: <entry key="SpeciesTaxId">4113,4081</entry>)
