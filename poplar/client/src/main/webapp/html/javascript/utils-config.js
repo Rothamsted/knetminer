5c9b1f2667357325b13ec7704e69ba1ab44d0504
KeywanHP <keywan.hassani-pak@.rothamsted.ac.uk>
2013-10-11 17:37:13 +0100

    add new parameters to configuration

--- poplar/client/src/main/webapp/html/javascript/utils-config.js
+++ poplar/client/src/main/webapp/html/javascript/utils-config.js
@@ -3,3 +3,6 @@ var graph_url = "http://ondex.rothamsted.ac.uk/poplar_data/graph/generated/";
 var applet_url = "http://ondex.rothamsted.ac.uk/applet-0.5.0/";
 	
 var species = 'Poplar Genome';
+
+var reference_genome = true;
+var multiorganisms = false;
