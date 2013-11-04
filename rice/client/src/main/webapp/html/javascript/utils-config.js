145aede79116156bc9c1d524bb12934f73f29701
KeywanHP <keywan.hassani-pak@.rothamsted.ac.uk>
2013-11-04 12:43:31 +0000

    remove old files and parameters

--- rice/client/src/main/webapp/html/javascript/utils-config.js
+++ rice/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,2 +1,8 @@
-var data_url = "http://ondex.rothamsted.ac.uk/poplar_data/annotation/generated/";
-var species = 'Poplar Genome';
+var data_url = "http://ondex.rothamsted.ac.uk/rice_data/annotation/generated/";
+var graph_url = "http://ondex.rothamsted.ac.uk/rice_data/graph/generated/";
+var applet_url = "http://ondex.rothamsted.ac.uk/applet-0.5.0/";
+	
+var species = 'Rice Genome';
+
+var reference_genome = false;
+var multiorganisms = false;
