69efea0bfe2227815c19c9bb8219660af3465865
maesch <maesch@web.de>
2013-11-20 14:43:55 +0000

    update release notes for barley, arabidopsis, potato and tomato

--- rice/client/src/main/webapp/html/javascript/utils-config.js
+++ rice/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,8 +1,6 @@
 var data_url = "http://ondex.rothamsted.ac.uk/barley_data/";
 var graph_url = "http://ondex.rothamsted.ac.uk/barley_data/";
 var applet_url = "http://ondex.rothamsted.ac.uk/applet-0.5.0/";
-	
 var species = 'Barley Genome';
-
-var reference_genome = false;
+var reference_genome = true;
 var multiorganisms = false;
