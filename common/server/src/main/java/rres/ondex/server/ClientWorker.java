de191c6686109b4da9ea116e468e0362f6f4eedf
maesch <maesch@web.de>
2014-01-08 15:06:40 +0000

    Change QTL maximum from 70 to 150 in ClientWorker.java

--- common/server/src/main/java/rres/ondex/server/ClientWorker.java
+++ common/server/src/main/java/rres/ondex/server/ClientWorker.java
@@ -311,7 +311,7 @@ public class ClientWorker implements Runnable {
 		List<QTL> qtlDB = ondexProvider.findQTL(keyword);
 		
 		//some logic to limit QTL size if too many are found
-		if(qtlDB.size() > 70){
+		if(qtlDB.size() > 150){
 			System.out.println("Too many QTL found, remove not significant ones...");
 			for(QTL q : qtlDB){
 				if(!q.getSignificance().equalsIgnoreCase("significant")){
