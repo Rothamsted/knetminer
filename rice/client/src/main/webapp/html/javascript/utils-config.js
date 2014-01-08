ab7864b4462f7deede3888143faab2490f494643
maesch <maesch@web.de>
2014-01-08 15:15:00 +0000

    changes in barley (gview, taxids, ...)

--- rice/client/src/main/webapp/html/javascript/utils-config.js
+++ rice/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,6 +1,5 @@
 var data_url = "http://ondex.rothamsted.ac.uk/barley_data/";
-var graph_url = "http://ondex.rothamsted.ac.uk/barley_data/";
 var applet_url = "http://ondex.rothamsted.ac.uk/applet-0.5.0/";
 var species = 'Barley Genome';
-var reference_genome = true;
+var reference_genome = false;
 var multiorganisms = false;
