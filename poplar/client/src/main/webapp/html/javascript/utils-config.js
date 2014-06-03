4270696c1882c8698f11baec55c3241723e43a69
maesch <maesch@web.de>
2014-06-03 11:27:19 +0100

    new release notes for plant organism

--- poplar/client/src/main/webapp/html/javascript/utils-config.js
+++ poplar/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,7 +1,5 @@
 var data_url = "http://ondex.rothamsted.ac.uk/poplar_data/";
 var applet_url = "http://ondex.rothamsted.ac.uk/applet-0.5.0/";
-	
 var species = 'Poplar Genome';
-
 var reference_genome = true;
 var multiorganisms = false;
