fcd3d3fc325fba229f016985554ab5e71adfb0ae
singha <singha@PC07663-ROTH.rothamsted.ac.uk>
2015-03-26 15:37:22 +0000

    cmt_boleracea_client_utils-config_data_url_edited

--- boleracea/client/src/main/webapp/html/javascript/utils-config.js
+++ boleracea/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,4 +1,5 @@
-var data_url = "https://ondex.rothamsted.ac.uk/boleracea_data/";
+//var data_url = "https://ondex.rothamsted.ac.uk/boleracea_data/";
+var data_url = "http://babvs08.rothamsted.ac.uk/boleracea_data/";
 var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
 var species = 'B. oleracea cv. TO1000';
 var reference_genome = true;
