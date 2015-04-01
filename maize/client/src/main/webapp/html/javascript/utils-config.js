c158d122ba477a9acc3e10ba8d27d43d74396f0e
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2015-04-01 17:42:07 +0100

    Maize_client edited

--- maize/client/src/main/webapp/html/javascript/utils-config.js
+++ maize/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,6 +1,6 @@
 //var data_url = "https://ondex.rothamsted.ac.uk/maize_data/";
 var data_url = "http://babvs12.rothamsted.ac.uk/maize_data/";
 var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
-var species = 'Maize (AGPv3) genome';
+var species = 'Maize genome';
 var reference_genome = true;
 var multiorganisms = false;
