ac9bbbd1a6a28afd9fef69a2fb32e820de601b38
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2015-09-09 18:25:45 +0100

    QTLNetMiner_Stats_for_Release_page

---
+++ common/client/src/main/webapp/html/javascript/showNetworkStats.js
@@ -0,0 +1,46 @@
+/*
+ * Function to read .tab file containing stats about the knowledge network & its mappings & to update 
+ * them in the release.html webpage.
+ */
+function fetchStats() {
+ var fileUrl= dataUrl +"Network_Stats.tab";
+	$.ajax({
+        url:fileUrl,
+        type:'GET',
+        dataType:'text',
+        async: true,
+        timeout: 1000000,
+        error: function(){						  
+        },
+        success: function(text){
+console.log("Stats>> response: "+ text);
+    		var resp= text.split("\n");
+                var resp= response.replace("<", "");
+                resp= resp.replace(">", "");
+                var totalGenes= fetchValue(resp[1]);
+                var totalConcepts= fetchValue(resp[2]);
+                var totalRelations= fetchValue(resp[3]);
+                var geneEvidenceConcepts= fetchValue(resp[4]);
+                var minValues= fetchValue(resp[6]);
+                var maxValues= fetchValue(resp[7]);
+                var avgValues= fetchValue(resp[8]);
+                
+                // Display stats data.
+                var statsText= "Graph Stats:</br>"+"<ul><li>Total number of genes: "+ totalGenes +
+                        "</li><li>Total concepts: "+ totalConcepts +"</li><li>Total Relations: "+ 
+                        totalRelations +"</li><li>Concept2Gene #mappings: "+ geneEvidenceConcepts +
+                        "</li><li>Size of the gene-evidence networks:<ul><li>Min.: "+ minValues +
+                        "</li><li>Max.: "+ maxValues +"</li><li>Average: "+ avgValues +"</li></ul>"+
+                        "</li></ul>";
+                
+                document.getElementByID('network_stats').innerHTML= statsText;
+	}
+    });
+}
+
+ function fetchValue(valText) {
+  var start= valText.indexOf(">");
+  var end= valText.indexOf("</");
+  var val= valText.substring(start+1, end);
+  return val;
+ }
