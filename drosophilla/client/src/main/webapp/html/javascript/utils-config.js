3c6344da75a3d3028f7259b2d15500720a388b6c
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2015-09-11 12:35:40 +0100

    QTLNetMiner_Drosophilla_instance

---
+++ drosophilla/client/src/main/webapp/html/javascript/utils-config.js
@@ -0,0 +1,6 @@
+var data_url = "https://ondex.rothamsted.ac.uk/drosophilla_data/";
+//var data_url = "http://babvs43.rothamsted.ac.uk/drosophilla_data/";
+var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
+var species = 'Maize genome';
+var reference_genome = true;
+var multiorganisms = false;
