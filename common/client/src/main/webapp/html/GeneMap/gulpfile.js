7c0dff5432cbdf81b4f08dc811821bd9d9ae90cb
Peter Norton <peter.norton@tessella.com>
2016-02-23 17:30:02 +0000

    Issue#11: added the gulp-load-plugins library and introduced a gulp configuration file.

--- common/client/src/main/webapp/html/GeneMap/gulpfile.js
+++ common/client/src/main/webapp/html/GeneMap/gulpfile.js
@@ -1,24 +1,21 @@
-/* globals require */
+/* globals require, -$ */
 
 // grab our gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util'),
-    jshint = require('gulp-jshint'),
-    jscs = require('gulp-jscs'),
-    gulpprint = require('gulp-print'),
-    gulpif = require('gulp-if'),
-    args = require('yargs').argv;
+    args = require('yargs').argv,
+    $ = require('gulp-load-plugins')({ lazy: true }),
+    config = require('./gulp.config')();
 
 gulp.task('vet', function () {
-  return gulp.src(['./src/**/*.js', './*.js'])
-    .pipe(gulpif(args.verbose, gulpprint()))
-    .pipe(jscs())
-    .pipe(jscs.reporter())
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+  return gulp.src(config.alljs)
+    .pipe($.if(args.verbose, $.print()))
+    .pipe($.jscs())
+    .pipe($.jscs.reporter())
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish', { verbose: true }));
 });
 
 // create a default task and just log a message
 gulp.task('default', function () {
-  return gutil.log('Gulp is running!');
+  return $.gutil.log('Gulp is running!');
 });
---
+++ common/client/src/main/webapp/html/GeneMap/gupl.config.js
@@ -0,0 +1,10 @@
+/* globals  module, -$ */
+
+module.exports = function() {
+  var config = {
+    // all the JavaScript files for this project
+    alljs: ['./src/**/*.js', './*.js'],
+  };
+
+  return config;
+};
