1cdf185f85c532c378f7f8881f3ca03d36bd4c0b
Peter Norton <peter.norton@tessella.com>
2016-02-23 15:37:17 +0000

    Issue#11: Added a gulp process to run static code analysis.

--- common/client/src/main/webapp/html/GeneMap/gulpfile.js
+++ common/client/src/main/webapp/html/GeneMap/gulpfile.js
@@ -1,17 +1,24 @@
-/* File: gulpfile.js */
+/* globals require */
 
 // grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    gulpprint = require('gulp-print'),
+    gulpif = require('gulp-if'),
+    args = require('yargs').argv;
 
-gulp.task('jshint', function() {
-  return gulp.src('src/**/*.js')
+gulp.task('vet', function () {
+  return gulp.src(['./src/**/*.js', './*.js'])
+    .pipe(gulpif(args.verbose, gulpprint()))
+    .pipe(jscs())
+    .pipe(jscs.reporter())
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
 });
 
 // create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+gulp.task('default', function () {
+  return gutil.log('Gulp is running!');
 });
