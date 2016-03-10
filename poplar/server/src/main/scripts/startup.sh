c33706f9c244331cbb563c1c23350dca2dc560d4
Peter Norton <peter.norton@tessella.com>
2016-03-10 14:10:55 +0000

    Added some missing depenancies to the pom.xml files.

--- poplar/server/src/main/scripts/startup.sh
+++ poplar/server/src/main/scripts/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-nohup /usr/java/latest/bin/java -Xmx12G -jar qtlnetminer-server.jar Poplar_v3_KB2014.oxl >> stdout.log 2>> stderr.log &
+nohup java -Xmx12G -jar qtlnetminer-server.jar Poplar_v3_KB2014.oxl >> stdout.log 2>> stderr.log &
