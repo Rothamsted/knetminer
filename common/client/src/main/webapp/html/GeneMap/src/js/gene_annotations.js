c279ff3db8710ebf90d5415dab89995ffc3474ad
Francis Newson <Francis.Newson@Tessella.com>
2016-07-05 09:09:06 +0100

    Seperate gene clustering code into separate file

--- common/client/src/main/webapp/html/GeneMap/src/js/gene_annotations.js
+++ common/client/src/main/webapp/html/GeneMap/src/js/gene_annotations.js
@@ -1,86 +1,5 @@
 var GENEMAP = GENEMAP || {};
 
-//Group genes into clusters for display
-//y is the yscale
-GENEMAP.GeneClusterer = function (y, userConfig){
-  var my = {};
-
-  var defaultConfig = {
-    annotationMarkerSize: 5,
-  };
-
-  var config = _.merge({}, defaultConfig, userConfig);
-
-  my.createNodesFromGenes = function(genes) {
-    var result = [];
-
-    //Return empty list if we have no genes
-    if (genes.length < 1 )
-    {
-      return result;
-    }
-
-    //Use precanned algorithm for clustering
-    //It return list of lists of midpoints
-    cluster_points = ss.ckmeans( genes.map( function(d){ return d.midpoint;} ), Math.min(6, genes.length) );
-
-    //Now we have to retrieve the genes at each midpoint, populating cluster_dict
-    var cluster_dict = []
-    for ( var iclus= 0 ; iclus < cluster_points.length ; iclus++ )
-    {
-      cluster_dict.push([]);
-    }
-
-    var in_cluster = function( gene, cluster ) {
-      for ( var iGene= 0 ; iGene < cluster.length ; iGene++ ){
-        if ( cluster[iGene] == gene.midpoint )
-        { return true;}
-      }
-      return false;
-    }
-
-    for ( var iGene = 0 ; iGene < genes.length ; iGene++ ) {
-
-      iCluster = cluster_points.findIndex( function(d){
-        return in_cluster(genes[iGene], d )});
-
-      cluster_dict[iCluster].push( genes[iGene]);
-    }
-
-
-    //loop over clusters and add nodes to the result
-    for ( var iclus= 0 ; iclus < cluster_dict.length ; iclus++ ) {
-      var genes = cluster_dict[iclus];
-
-      //for small clusters, add individual genes
-      if (genes.length < 2) {
-        for ( var iGene = 0 ; iGene < genes.length ; iGene ++){
-         result.push( new labella.Node(
-          y(genes[iGene].midpoint), config.annotationMarkerSize, genes[iGene]) );
-      }
-      }
-      //for large clusters, add 1 node for all of them
-      else {
-        var average_midpoint = genes.reduce( function(sum, current){
-              return sum + current.midpoint;
-            }, 0 ) / genes.length;
-
-        var genes_object = {
-          genes_list : genes,
-          midpoint : average_midpoint,
-          type : "geneslist"
-        };
-
-       result.push(  new labella.Node(
-            y( average_midpoint), config.annotationMarkerSize, genes_object ) );
-      }
-    }
-
-    return result;
-  };
-
-  return my;
-};
 
 GENEMAP.GeneAnnotations = function (userConfig) {
   var defaultConfig = {
@@ -113,8 +32,12 @@ GENEMAP.GeneAnnotations = function (userConfig) {
 
     var y = buildYScale();
 
-    var geneClusterer = GENEMAP.GeneClusterer(y, _.pick(config, ['annotationMarkerSize']));
-    var nodes = geneClusterer.createNodesFromGenes( chromosome.annotations.genes )
+    var geneClusterer = GENEMAP.GeneClusterer();
+    var gene_objects = geneClusterer.createNodesFromGenes( chromosome.annotations.genes )
+    var nodes = gene_objects.map( function(d){
+      return new labella.Node(
+          y(d.midpoint), config.annotationMarkerSize, d ); }
+    );
 
     var force = new labella.Force({
       nodeSpacing: 3,
---
+++ common/client/src/main/webapp/html/GeneMap/src/js/gene_clusterer.js
@@ -0,0 +1,79 @@
+var GENEMAP = GENEMAP || {};
+
+//Group genes into clusters for display
+//y is the yscale
+GENEMAP.GeneClusterer = function (userConfig){
+    var my = {};
+
+    var defaultConfig = {};
+
+    var config = _.merge({}, defaultConfig, userConfig);
+
+    my.createNodesFromGenes = function(genes) {
+        var result = [];
+
+        //Return empty list if we have no genes
+        if (genes.length < 1 )
+        {
+            return result;
+        }
+
+        //Use precanned algorithm for clustering
+        //It return list of lists of midpoints
+        cluster_points = ss.ckmeans( genes.map( function(d){ return d.midpoint;} ), Math.min(6, genes.length) );
+
+        //Now we have to retrieve the genes at each midpoint, populating cluster_dict
+        var cluster_dict = []
+        for ( var iclus= 0 ; iclus < cluster_points.length ; iclus++ )
+        {
+            cluster_dict.push([]);
+        }
+
+        var in_cluster = function( gene, cluster ) {
+            for ( var iGene= 0 ; iGene < cluster.length ; iGene++ ){
+                if ( cluster[iGene] == gene.midpoint )
+                { return true;}
+            }
+            return false;
+        }
+
+        for ( var iGene = 0 ; iGene < genes.length ; iGene++ ) {
+
+            iCluster = cluster_points.findIndex( function(d){
+                return in_cluster(genes[iGene], d )});
+
+            cluster_dict[iCluster].push( genes[iGene]);
+        }
+
+
+        //loop over clusters and add nodes to the result
+        for ( var iclus= 0 ; iclus < cluster_dict.length ; iclus++ ) {
+            var genes = cluster_dict[iclus];
+
+            //for small clusters, add individual genes
+            if (genes.length < 2) {
+                for ( var iGene = 0 ; iGene < genes.length ; iGene ++){
+                    result.push( genes[iGene]);
+                }
+            }
+            //for large clusters, add 1 node to hold all of them
+            else {
+                var average_midpoint = genes.reduce( function(sum, current){
+                        return sum + current.midpoint;
+                    }, 0 ) / genes.length;
+
+                var genes_collection = {
+                    genes_list : genes,
+                    midpoint : average_midpoint,
+                    type : "geneslist"
+                };
+
+                result.push( genes_collection);
+            }
+        }
+
+        return result;
+    };
+
+    return my;
+};
