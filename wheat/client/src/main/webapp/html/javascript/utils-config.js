875e0ad6ceed158e4c6fcdd903a8c88f7adff782
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2016-08-20 16:00:50 +0100

    Wheat_utilsConfig_disabled_MapView_for_new_CS_Wheat_release_oxl

--- wheat/client/src/main/webapp/html/javascript/utils-config.js
+++ wheat/client/src/main/webapp/html/javascript/utils-config.js
@@ -2,7 +2,7 @@ var data_url = "https://ondex.rothamsted.ac.uk/wheat_data/";
 // var data_url = "http://qtlnetminer-test.rothamsted.ac.uk/wheat_data/"; // now, using HTTP instead of HTTPS
 var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
 var species = 'Wheat Genome (IWGSP1, MIPSv2.1)';
-var reference_genome = true; //true if you are providing a reference genome
+var reference_genome = false;//true; //true if you are providing a reference genome
 var multiorganisms = false; //true if you specified more than one taxid in the server file config.xml (ie: <entry key="SpeciesTaxId">4113,4081</entry>)
 var gviewer = true;
 var longest_chr = 207000000;
