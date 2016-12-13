133f5598949b4aa8c63b973090c1f6c4ad64e8db
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2016-12-13 13:44:34 +0000

    Sviminalis MapView disabled in utils-config.js

--- sviminalis/client/src/main/webapp/html/javascript/utils-config.js
+++ sviminalis/client/src/main/webapp/html/javascript/utils-config.js
@@ -3,6 +3,6 @@ var data_url = "http://qtlnetminer-test.rothamsted.ac.uk/sviminalis_data/"; // n
 //var data_url = "http://maize-dev.rothamsted.ac.uk/sviminalis_data/"; // test on maize-dev
 var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
 var species = "Sviminalis Genome";
-var reference_genome = true; //true if you are providing a reference genome
+var reference_genome = false; //true if you are providing a reference genome
 var multiorganisms = false; //true if you specified more than one taxid in the server file config.xml (ie: <entry key="SpeciesTaxId">4113,4081</entry>)
 var species_name = 'Salix viminalis';
