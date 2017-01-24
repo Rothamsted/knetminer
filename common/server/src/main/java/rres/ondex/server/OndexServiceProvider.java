21f63d56f2dcb6323230ed1884338a813fdb830e
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2017-01-24 18:50:00 +0000

    added detailed stats breakdown table

--- common/server/src/main/java/rres/ondex/server/OndexServiceProvider.java
+++ common/server/src/main/java/rres/ondex/server/OndexServiceProvider.java
@@ -241,7 +241,6 @@ public class OndexServiceProvider {
 			int totalConcepts = graph.getConcepts().size();
 			int totalRelations = graph.getRelations().size();
 			int geneEvidenceConcepts = mapConcept2Genes.size();
-
 			minValues = mapGene2Concepts.get(mapGene2Concepts.keySet().toArray()[0]).size(); // initial
 																								// value
 			/*
@@ -253,11 +252,8 @@ public class OndexServiceProvider {
 			while (iterator.hasNext()) {
 				Map.Entry mEntry = (Map.Entry) iterator.next();
 				HashSet<Integer> value = (HashSet<Integer>) mEntry.getValue(); // Value
-																				// (as
-																				// a
-																				// HashSet<Integer>).
+                                																				// HashSet<Integer>).
 				int number_of_values = value.size(); // size of the values
-														// HashSet for this Key.
 
 				if (number_of_values < minValues) {
 					minValues = number_of_values;
@@ -281,8 +277,8 @@ public class OndexServiceProvider {
 			 * System.out.println("2) Total concepts: "+ totalConcepts);
 			 * System.out.println("3) Total Relations: "+ totalRelations);
 			 * System.out.println("4) Concept2Gene #mappings: "+
-			 * geneEvidenceConcepts); System.out.
-			 * println("5) Min., Max., Average size of gene-evidence networks: "
+			 * geneEvidenceConcepts); 
+                         * System.out.println("5) Min., Max., Average size of gene-evidence networks: "
 			 * + minValues +", "+ maxValues +", "+ avgValues);
 			 */
 
@@ -300,6 +296,23 @@ public class OndexServiceProvider {
 			sb.append("<avgSize>").append(avgValues).append("</avgSize>\n");
 			sb.append("</evidenceNetworkSizes>\n");
 			sb.append("</stats>");
+
+                        // Display table breakdown of all conceptClasses in network
+			sb.append("<conceptClasses>\n");
+                        Set<ConceptClass> conceptClasses= (TreeSet<ConceptClass>) graph.getMetaData().getConceptClasses();
+                        for (ConceptClass con_class : conceptClasses) {
+                             if(graph.getConceptsOfConceptClass(con_class).size() >0) {
+                                String conID= con_class.getId();
+                                if(conID.equalsIgnoreCase("Path")) {
+                                   conID= "Pathway";
+                                  }
+                                else if(conID.equalsIgnoreCase("Comp")) {
+                                   conID= "SNP";
+                                  }
+			        sb.append("<cc_count>").append(con_class.getId()).append("=").append(graph.getConceptsOfConceptClass(con_class).size()).append("</cc_count>\n");
+                               }
+                            }
+			sb.append("</conceptClasses>");
 
 			// Update the file storing the latest Stats data.
 			BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
