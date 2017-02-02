9db838165b76c5f7f66fde905933e7d665f7fc1e
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2017-02-02 17:09:17 +0000

    Wheat client config test

--- wheat/client/src/main/webapp/html/javascript/utils-config.js
+++ wheat/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,5 +1,6 @@
 var data_url = "http://knetminer.rothamsted.ac.uk/wheat_data/";
 //var data_url = "http://qtlnetminer-test.rothamsted.ac.uk/wheat_data/"; // now, using HTTP instead of HTTPS
+//var data_url = "http://maize-dev.rothamsted.ac.uk/wheat_data/";
 var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
 var species = 'Wheat Genome (IWGSP1, MIPSv2.1)';
 var reference_genome = /*true*/false; //true if you are providing a reference genome
