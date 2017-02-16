f913fd9e50ba6fd5478a5f9005dd71842217abcc
Ajit Singh <ajitprataps@yahoo.com>
2017-02-16 11:38:49 +0000

    HumanDisease releaseNotes edited

--- humanDisease/client/src/main/webapp/html/javascript/utils-config.js
+++ humanDisease/client/src/main/webapp/html/javascript/utils-config.js
@@ -6,4 +6,4 @@ var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
 var species = "DiseaseKnetMiner Human";
 var reference_genome = true; //true if you are providing a reference genome
 var multiorganisms = false; //true if you specified more than one taxid in the server file config.xml (ie: <entry key="SpeciesTaxId">4113,4081</entry>)
-var species_name = 'DiseaseKnetMiner-Human';
+var species_name = 'DiseaseKnetMiner Human';
