90426c358aebf879077cb1eabf8b95fdf40d1d47
mistrym <mistrym@PC09066-ROTH.rothamsted.ac.uk>
2017-06-01 14:30:33 +0100

    edits

--- tomato/server/src/main/scripts/startup.sh
+++ tomato/server/src/main/scripts/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-nohup /usr/java/latest/bin/java -Xmx16G -jar qtlnetminer-server.jar SOLNetKB2013.oxl >> stdout.log 2>> stderr.log &
+nohup /usr/java/latest/bin/java -Xmx12G -jar qtlnetminer-server.jar SolKNET.oxl >> stdout.log 2>> stderr.log &
