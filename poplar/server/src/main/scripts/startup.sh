093388b72f629a0c39f9fdbaa3705e9501cf68cd
mistrym <mistrym@PC09066-ROTH.rothamsted.ac.uk>
2017-06-07 11:55:15 +0100

    startup.sh edits

--- poplar/server/src/main/scripts/startup.sh
+++ poplar/server/src/main/scripts/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-nohup java -Xmx12G -jar qtlnetminer-server.jar PoplarKNET.oxl >> stdout.log 2>> stderr.log &
+nohup java -Xmx12G -jar qtlnetminer-server.jar PoplarKNET_201705.oxl >> stdout.log 2>> stderr.log &
