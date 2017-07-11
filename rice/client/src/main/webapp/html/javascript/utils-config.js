5df45de2fa30070f3967d128991fa02844844714
singha <singha@PC09466-ROTH.rothamsted.ac.uk>
2017-07-11 15:22:30 +0100

    Rice config client

--- rice/client/src/main/webapp/html/javascript/utils-config.js
+++ rice/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,7 +1,5 @@
 var data_url = "http://knetminer.rothamsted.ac.uk/rice_data/";
-var species = 'Barley Genome';
+var species = 'Rice Genome';
 var reference_genome = true;
 var multiorganisms = false;
-var longest_chr = 196087864; //its needed if the first chromosome is not the longest to scale the chromosome view in gviewer
-//var longest_chr = 170;
-var species_name = 'Oryza sativa';
+var species_name = 'Oryza sativa Japonica';
