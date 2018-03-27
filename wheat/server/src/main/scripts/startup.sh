c375d9a4f3be9eaf830752e8f7f96882644d381c
singha <singha@PC09466-ROTH.rothamsted.ac.uk>
2018-03-27 15:12:08 +0100

    wheat new oxl with tilling, CoExp and PO

--- wheat/server/src/main/scripts/startup.sh
+++ wheat/server/src/main/scripts/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 umask 022
-nohup java -Xmx28G -jar qtlnetminer-server.jar WheatKNET.oxl >> stdout.log 2>> stderr.log &
+nohup java -Xmx28G -jar qtlnetminer-server.jar Wheat_v1.0_noGenie.oxl >> stdout.log 2>> stderr.log &
