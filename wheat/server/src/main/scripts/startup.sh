ef03437f72cdefc2f05bd10d736a009ff53b81a0
singha <singha@PC09466-ROTH.rothamsted.ac.uk>
2018-03-28 17:41:10 +0100

    oxl updated

--- wheat/server/src/main/scripts/startup.sh
+++ wheat/server/src/main/scripts/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 umask 022
-nohup java -Xmx30G -jar qtlnetminer-server.jar WheatKNET_v1.0_noGenie.oxl >> stdout.log 2>> stderr.log &
+nohup java -Xmx30G -jar qtlnetminer-server.jar WheatKNET_v1.0.oxl >> stdout.log 2>> stderr.log &
