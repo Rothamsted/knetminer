94cde80e98f3c51126cde10a8d3547fd7bd33b56
dicknetherlands <richard@nfventures.co.uk>
2018-05-22 11:04:26 +0100

    Converted chicken.

--- chicken/client/src/main/webapp/html/javascript/utils-config.js
+++ chicken/client/src/main/webapp/html/javascript/utils-config.js
@@ -1,6 +1,5 @@
-var data_url = "http://knetminer.rothamsted.ac.uk/chicken_data/";
+var api_url = "http://knetminer.rothamsted.ac.uk/data/chicken/";
 //var data_url = "http://qtlnetminer-test.rothamsted.ac.uk/chicken_data/"; // now, using HTTP instead of HTTPS
-var applet_url = "https://ondex.rothamsted.ac.uk/OndexWebBeta/";
 var species = 'Chicken genome';
 var reference_genome = true;
 var multiorganisms = false;
