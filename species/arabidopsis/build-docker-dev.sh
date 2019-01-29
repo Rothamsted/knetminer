2460744c80147b388168722d8136db5622802bdc
Ajit Singh <ajitprataps@yahoo.com>
2019-01-29 11:54:31 +0000

    oxl

--- species/arabidopsis/build-docker-dev.sh
+++ species/arabidopsis/build-docker-dev.sh
@@ -25,7 +25,7 @@ docker image build \
     	--build-arg description="Arabidopsis Knetminer" \
     	--build-arg reference_genome=true \
   --build-arg species_dir="species/$SPECIES_DIR" \
-  --build-arg oxl_file="data_ara.oxl" \
+  --build-arg oxl_file="ArabidopsisKNET.oxl" \
   --build-arg knetminer_port=8080 \
   --squash -t $IMAGE_NAME \
   -f common/quickstart/Dockerfile-dev .
