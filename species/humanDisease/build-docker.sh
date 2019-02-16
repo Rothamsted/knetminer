ffc668474f5d268384e4223d61de673fe1eeff3a
Ajit Singh <ajitprataps@yahoo.com>
2019-02-16 09:23:12 +0000

    edits

--- species/humanDisease/build-docker.sh
+++ species/humanDisease/build-docker.sh
@@ -11,7 +11,6 @@ docker image build \
     	--build-arg keywords="human, humanDisease, knetminer, quickstart, demo" \
     	--build-arg description="HumanDisease Knetminer" \
     	--build-arg reference_genome=true \
-  --build-arg git_branch=`git branch | grep \* | cut -d ' ' -f2` \
   --build-arg oxl_file="NeuroDiseaseKnet.oxl" \
   --build-arg knetminer_port=8080 \
   --squash -t $IMAGE_NAME \
