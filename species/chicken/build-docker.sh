3f643df6da7015d9791e929ddcd7f48f628fc50c
Ajit Singh <ajitprataps@yahoo.com>
2019-02-16 09:22:24 +0000

    Update build-docker.sh

--- species/chicken/build-docker.sh
+++ species/chicken/build-docker.sh
@@ -11,7 +11,6 @@ docker image build \
     	--build-arg keywords="chicken, gallus, knetminer, quickstart, demo" \
     	--build-arg description="Chicken Knetminer" \
     	--build-arg reference_genome=true \
-  --build-arg git_branch=`git branch | grep \* | cut -d ' ' -f2` \
   --build-arg oxl_file="ChickenKNET.oxl" \
   --build-arg knetminer_port=8080 \
   --squash -t $IMAGE_NAME \
