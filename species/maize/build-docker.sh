abca5036ea4c25234186d0d6070ebc4609ecaf38
Ajit Singh <ajitprataps@yahoo.com>
2019-02-16 09:23:31 +0000

    maize git_branch; set in Dockerfile-local instead

--- species/maize/build-docker.sh
+++ species/maize/build-docker.sh
@@ -11,7 +11,6 @@ docker image build \
     	--build-arg keywords="maize, z.mays, knetminer, quickstart, demo" \
     	--build-arg description="Maize Knetminer" \
     	--build-arg reference_genome=true \
-  --build-arg git_branch=`git branch | grep \* | cut -d ' ' -f2` \
   --build-arg oxl_file="MaizeKNET.oxl" \
   --build-arg knetminer_port=8080 \
   --squash -t $IMAGE_NAME \
