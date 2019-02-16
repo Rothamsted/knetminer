292098f1793dfe4f33c2922ba8fe1a087026b6cb
Ajit Singh <ajitprataps@yahoo.com>
2019-02-16 09:22:31 +0000

    Update build-docker.sh

--- species/cicer/build-docker.sh
+++ species/cicer/build-docker.sh
@@ -11,7 +11,6 @@ docker image build \
     	--build-arg keywords="cicer, c.arietinum, knetminer, quickstart, demo" \
     	--build-arg description="Cicer Knetminer" \
     	--build-arg reference_genome=true \
-  --build-arg git_branch=`git branch | grep \* | cut -d ' ' -f2` \
   --build-arg oxl_file="CicerKNET.oxl" \
   --build-arg knetminer_port=8080 \
   --squash -t $IMAGE_NAME \
