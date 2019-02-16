959874a7526b210bfc7ca359f9a9b81e2d71166a
Ajit Singh <ajitprataps@yahoo.com>
2019-02-16 09:24:39 +0000

    wheat omit git_branch; set in Dockerfile-local

--- species/wheat/build-docker.sh
+++ species/wheat/build-docker.sh
@@ -11,7 +11,6 @@ docker image build \
     	--build-arg keywords="wheat, t.aestivum, knetminer, quickstart, demo" \
     	--build-arg description="Knetminer Wheat" \
     	--build-arg reference_genome=true \
-  --build-arg git_branch=`git branch | grep \* | cut -d ' ' -f2` \
   --build-arg oxl_file="WheatKNET_v42.oxl" \
   --build-arg knetminer_port=8080 \
   --squash -t $IMAGE_NAME \
