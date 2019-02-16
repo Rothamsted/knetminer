9d87d4e777cd04b3b5bc91cd857bacff89523e21
Ajit Singh <ajitprataps@yahoo.com>
2019-02-16 09:23:57 +0000

    Update build-docker.sh

--- species/rice/build-docker.sh
+++ species/rice/build-docker.sh
@@ -11,7 +11,6 @@ docker image build \
     	--build-arg keywords="rice, o.sativa, knetminer, quickstart, demo" \
     	--build-arg description="Rice Knetminer" \
     	--build-arg reference_genome=true \
-  --build-arg git_branch=`git branch | grep \* | cut -d ' ' -f2` \
   --build-arg oxl_file="RiceKNET.oxl" \
   --build-arg knetminer_port=8081 \
   --squash -t $IMAGE_NAME \
