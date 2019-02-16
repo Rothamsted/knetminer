9862ee5dc389d39a258af83f3154e7ff1db8ff15
Ajit Singh <ajitprataps@yahoo.com>
2019-02-16 09:23:44 +0000

    Update build-docker.sh

--- species/poplar/build-docker.sh
+++ species/poplar/build-docker.sh
@@ -11,7 +11,6 @@ docker image build \
     	--build-arg keywords="poplar, p.trichocarpa, knetminer, quickstart, demo" \
     	--build-arg description="Poplar Knetminer" \
     	--build-arg reference_genome=true \
-  --build-arg git_branch=`git branch | grep \* | cut -d ' ' -f2` \
   --build-arg oxl_file="PoplarKNET.oxl" \
   --build-arg knetminer_port=8080 \
   --squash -t $IMAGE_NAME \
