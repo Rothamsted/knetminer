86258fb23f65ddde45d6b1cd665acccddc9324fe
Marco Brandizi <marco.brandizi@gmail.com>
2019-02-25 12:44:53 +0000

    Merging from branch, dirs moves to align with the new structure.

--- vagrant_files/install_tomcat.sh
+++
@@ -1,3 +0,0 @@
-wget http://mirror.ox.ac.uk/sites/rsync.apache.org/tomcat/tomcat-8/v8.0.32/bin/apache-tomcat-8.0.32.tar.gz
-tar xvzf apache-tomcat-8.0.32.tar.gz
-sudo mv apache-tomcat-8.0.32 /opt/tomcat
