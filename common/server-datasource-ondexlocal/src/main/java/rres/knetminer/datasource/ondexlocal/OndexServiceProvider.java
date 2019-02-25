47ea5d375872133ee667180fea2584688d762f9e
pakk <pakk@PC09465-ROTH.rothamsted.ac.uk>
2019-02-25 15:59:14 +0000

    Tidy up highlightPath method

--- common/server-datasource-ondexlocal/src/main/java/rres/knetminer/datasource/ondexlocal/OndexServiceProvider.java
+++ common/server-datasource-ondexlocal/src/main/java/rres/knetminer/datasource/ondexlocal/OndexServiceProvider.java
@@ -1624,6 +1624,13 @@ public class OndexServiceProvider {
         Set<ONDEXConcept> cons = path.getAllConcepts();
         Set<ONDEXRelation> rels = path.getAllRelations();
         
+        // seed gene should always be visible, flagged and bigger
+        if (geneNodeClone.getAttribute(attVisible) == null) {
+        	geneNodeClone.createAttribute(attFlagged, true, false);
+        	geneNodeClone.createAttribute(attVisible, true, false);
+        	geneNodeClone.createAttribute(attSize, new Integer(80), false);
+        }
+        
         // set all concepts to visible if filtering is turned off
         // OR filter is turned on and end node is of specific type
         if(!doFilter || (doFilter && ccFilter.contains(endNodeClone.getOfType()))){
@@ -1635,19 +1642,6 @@ public class OndexServiceProvider {
                 } 
             }
         }
-        
-        // seed gene should always be flagged
-        if (geneNodeClone.getAttribute(attFlagged) == null) {
-        	geneNodeClone.createAttribute(attFlagged, true, false);
-        }
-        
-        // seed gene should always be bigger 
-        if(geneNodeClone.getAttribute(attSize) == null){
-        	geneNodeClone.createAttribute(attSize, new Integer(80), false);
-        }else{
-        	geneNodeClone.getAttribute(attSize).setValue(new Integer(80));
-        }
-        
         
         // set all relations to visible if filtering is turned off
         // OR filter is turned on and end node is of specific type
