ff52587760721dfc11a12ab63f735fd6c4dc6af6
Marco Brandizi <marco.brandizi@gmail.com>
2019-05-07 16:25:10 +0200

    Doing the conversion to the new Docker config.

--- common/quickstart/docker-build-helper.sh
+++ common/quickstart/docker-build-helper.sh
@@ -11,25 +11,25 @@ knet_tomcat_pwd="$3"
 
 knet_web_dir="$knet_cfg_dir/web"
 
+
 echo -e "\n\n\tUpdating base environment\n"
 mkdir --parents "$knet_web_dir"
-for dir in '../..' common aratiny
-do
-	cd $dir
-	mvn $MAVEN_ARGS -N install
-done
+cd ../..
+mvn dependency:resolve
 
 
 # Build the ws (API) and put it under Tomcat
 echo -e "\n\n\tBuilding the ws app\n"
-cd aratiny-ws
+cd common/aratiny/aratiny-ws
 mvn clean install $MAVEN_ARGS -DskipTests -DskipITs
 cp -f target/aratiny-ws.war "$knet_tomcat_home/webapps/ws.war"
+
 
 # The client's war stays in the volume, linked under Tomcat. This way the docker runtime script can decide
 # if it has to be rebuilt
 #
 ln -s -f "$knet_web_dir/client.war" "$knet_tomcat_home/webapps/client.war"
+
 
 # Useful during dev
 if [ "$knet_tomcat_pwd" != '' ] && [ ! -e ./tomcat-users.xml.knetminer-bkp ]; then
