324b45aff09f211113c89a6cd2b1ae2d8c1c9ac8
root <root@babvs72.rothamsted.ac.uk>
2019-05-23 23:15:11 +0100

    Moving Docker host volumes in RRes deployment.

--- common/quickstart/docker-run-rres-test.sh
+++ common/quickstart/docker-run-rres-test.sh
@@ -3,16 +3,17 @@
 # ./docker-run-rres-test.sh arabidopsis # arabidopsis
 # arabidopsis Tomcat on 8090 (host port) Neo4j traverser, bolt port on 7687 (server is babvs65)
 # ./docker-run-rres-test.sh arabidopsis 8090 7687 
-# wheat, Tomcat on 9091, Neo4j on 7688, 16GB of RAM handed to the container
-# ./docker-run-rres-test.sh wheat 8091 7688 16
+# wheat, Tomcat on 9091, Neo4j on 7688, 20GB of RAM handed to the container
+# ./docker-run-rres-test.sh wheat 8091 7688 20
 #
 specie=$1 # Call me with the specie id/directory in species/, eg, 'arabidopsis'
 http_port=$2 # The host port to map to the 8080/Tomcat in the container
 bolt_port=$3 # The Neo4j bolt port, if not defined, runs the traditional version, not Neo4j
 memory=${4:-12} # The memory to give to the container, in GB, 12 is the default.
 
-export KNET_HOST_DATA_DIR=/root/knetminer-test/data/$specie/data
-export KNET_HOST_CONFIG_DIR=/root/knetminer-test/data/$specie/config
+host_data_root_dir="/var/lib/docker/knetminer-datasets/$specie"
+export KNET_HOST_DATA_DIR="$host_data_root_dir/data"
+export KNET_HOST_CONFIG_DIR="$host_data_root_dir/config"
 [ "$bolt_port" == '' ] || export KNET_IS_NEO4J='neo4j'
 #DEBUG export KNET_NEO4J_URL='bolt://192.168.1.100:17690'
 export KNET_NEO4J_URL="bolt://babvs65.rothamsted.ac.uk:$bolt_port"
