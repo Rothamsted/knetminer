08d561f694ad716d03bfdf79b791aef862be3ae2
root <root@babvs72.rothamsted.ac.uk>
2019-05-23 14:06:48 +0100

    More fixes to the Docker launching script.

--- common/quickstart/docker-run-rres-test.sh
+++ common/quickstart/docker-run-rres-test.sh
@@ -1,6 +1,7 @@
 specie=$1 # Call me with the specie id/directory in species/, eg, 'arabidopsis'
 http_port=$2 # The host port to map to the 8080/Tomcat in the container
 bolt_port=$3 # The Neo4j bolt port, if not defined, runs the traditional version, not Neo4j
+memory=${4:-12} # The memory to give to the container, in GB, 12 is the default.
 
 export KNET_HOST_DATA_DIR=/root/knetminer-test/data/$specie/data
 export KNET_HOST_CONFIG_DIR=/root/knetminer-test/data/$specie/config
@@ -12,7 +13,7 @@ export KNET_NEO4J_PWD='rouser'
 #DEBUG, to get code from your sandbox: export KNET_HOST_CODEBASE_DIR=/Users/brandizi/Documents/Work/RRes/ondex_git
 
 export JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1"
-export KNET_DOCKER_OPTS="--memory 12G --env JAVA_TOOL_OPTIONS -p $http_port:8080 --name $specie"
+export KNET_DOCKER_OPTS="--memory ${memory}G --env JAVA_TOOL_OPTIONS -p $http_port:8080 --name $specie"
 #export KNET_DOCKER_OPTS="$KNET_DOCKER_OPTS -it"
 export KNET_DOCKER_OPTS="$KNET_DOCKER_OPTS --detach"
 
