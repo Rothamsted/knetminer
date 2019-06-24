b2ea6e285d74a213dcab4f6a9a77fcf733b144d4
root <root@babvs72.rothamsted.ac.uk>
2019-06-24 18:00:27 +0100

    Adding profiling options to launching script.

--- common/quickstart/docker-run-rres-test.sh
+++ common/quickstart/docker-run-rres-test.sh
@@ -21,10 +21,24 @@ export KNET_NEO4J_USER='rouser'
 export KNET_NEO4J_PWD='rouser'
 #DEBUG, to get code from your sandbox: export KNET_HOST_CODEBASE_DIR=/Users/brandizi/Documents/Work/RRes/ondex_git
 
+# Use tall the memory you find
 export JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1"
+
+# Profiling (requires -p 5005:5005 passed to Docker) 
+#export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.ssl=false
+#                    -Dcom.sun.management.jmxremote.authenticate=false
+#                    -Dcom.sun.management.jmxremote.port=9098
+#                    -Dcom.sun.management.jmxremote.rmi.port=9099
+#                    -Djava.rmi.server.hostname=localhost
+#                    -Dcom.sun.management.jmxremote.local.only=false"
+
 export KNET_DOCKER_OPTS="--memory ${memory}G --env JAVA_TOOL_OPTIONS -p $http_port:8080 --name $specie"
 #export KNET_DOCKER_OPTS="$KNET_DOCKER_OPTS -it"
 export KNET_DOCKER_OPTS="$KNET_DOCKER_OPTS --detach"
 
+# Profiling (See above)
+#export KNET_DOCKER_OPTS="$KNET_DOCKER_OPTS -p 9098:9098 -p 9099:9099"
+
 cd "$(dirname $0)"
 ./docker-run.sh "$specie"
+
