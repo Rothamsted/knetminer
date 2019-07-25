4c621570043c34e4ceaf9e1037b8b3f9efd23658
singha <singha@PC09466-ROTH.rothamsted.ac.uk>
2019-07-25 15:40:20 +0100

    clickable evidences legend to show or hide non-matching terms

--- common/client-base/src/main/webapp/html/javascript/evidence_legend.js
+++ common/client-base/src/main/webapp/html/javascript/evidence_legend.js
@@ -10,7 +10,6 @@
       var evi= evi_view[i].split("\t")[0].trim();
       if(evi !== "") { summaryArr.push(evi); }
      }
-console.log("summaryArr: "+ summaryArr);
 
   var evi_legend= new Map();
   // Iterate through evidence types and get counts for each evidence Concept Type.
@@ -20,11 +19,9 @@ console.log("summaryArr: "+ summaryArr);
           if(evi_legend.has(eviType)) {
              var old_count= evi_legend.get(eviType);
              evi_legend.set(eviType, old_count+1);
-             console.log("\t EvidencesLegend: updated count for eviType: "+ eviType +" to= "+ evi_legend.get(eviType));
             }
           else { // add new evidence type to Map
               evi_legend.set(eviType, 1);
-              console.log("\t EvidencesLegend: added new eviType: "+ eviType);
           }
      });
 
@@ -33,8 +30,8 @@ console.log("summaryArr: "+ summaryArr);
   var summaryText = '';
   evi_legend.forEach(function(value, key, map) {
       var contype= key.trim();
-      console.log("key, value: "+ key +", "+ value);
       summaryText = summaryText + '<div class="evidenceSummaryItem"><div class="evidence_item evidence_item_' + key + '" onclick=filterEvidenceTableByType("'+contype+'"); title="' + key + '"></div>' + value + '</div>';
+    //  summaryText = summaryText + '<div class="evidenceSummaryItem"><div class="evidence_item evidence_item_' + key + '" onclick=filterEvidenceTableByType("'+contype+'"); id="evi_' + key + '" title="' + key + '"></div>' + value + '</div>';
   });
 
   legend= legend + summaryText + '<input id="revertEvidenceView" type="button" value="" class="unhover" title= "Revert all filtering changes">'+'</div>';
@@ -49,25 +46,34 @@ console.log("summaryArr: "+ summaryArr);
   console.log("Evidence View legend: filterEvidenceTableByType= "+ key);
   // Check which Tab user is on: Gene View or Evidence View
   try {
+      // disable clicked button
+    //  $('#evi_'+key).prop('disabled',true).css('opacity',0.5);
+    //  if($('#evi_'+key).is(':disabled')) { console.log("already disabled..."); }
+    //  else {
       if ($('#evidenceTable').css('display') === 'block') {
         // get tbody
         //  $('#tablesorterEvidence').children('tbody');
           var evTable= /*$('#tablesorterEvidence');*/ document.getElementById("tablesorterEvidence");
 	  var rowLength= evTable.rows.length;
-	  console.log("EvidenceTable: rows= "+ rowLength +", columns= "+ evTable.rows[0].cells.length);
 	  for(var i=1; i < rowLength; i++) { // i=1 to skip title row
 	      var currentRow= evTable.rows.item(i);
 	      // get cells of current row
 		  var ev_cells = currentRow.cells;
 		  var evType= ev_cells.item(1).innerHTML; // Evidences
-		  console.log("EvidenceTable: evType: "+ evType +"; evName: "+ ev_cells.item(2).innerHTML);
 		  if(!evType.includes(key)) {
-		  console.log("HIDE evType: "+ evType);
-		     // hide row
-			 currentRow.style.display= 'none';
+                      // if evidences of this type are shown, hide those table rows.
+                      console.log("\t HIDE: evType: "+ evType);
+                      currentRow.style.display= 'none';
 		    }
+                  else { // Else, show them to allow users to visualize multiple evidence types together
+                      if (currentRow.style.display === 'none') {
+                          console.log("\t SHOW: evType: "+ evType);
+			  currentRow.style.display= 'table-row';
+                         }
+                    }
 	      }
            }
+       //}
      }
      catch (err) {
          var errorMsg = err.stack + ":::" + err.name + ":::" + err.message;
