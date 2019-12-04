6b4a841af72da9a14aa5f75ff0ae99876cdb740c
Marco Brandizi <marco.brandizi@gmail.com>
2019-12-04 12:27:10 +0000

    Adding branch building to Travis.

--- travis-build.sh
+++ travis-build.sh
@@ -4,7 +4,7 @@
 
 set -e # Fail fast upon the first error
 
-[[ "${TRAVIS_PULL_REQUEST}" == "false" ]] && goal='install' || goal='deploy'
+[[ "${TRAVIS_PULL_REQUEST}" == "true" ]] && goal='install' || goal='deploy'
 
 # You need --quiet, Travis doesn't like too big logs.
 echo -e "\n\n\t MAVEN GOAL: $goal"
