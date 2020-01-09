44d7b4c6e91ad977fcc7b07a88f6932972ebbf1c
Marco Brandizi <marco.brandizi@gmail.com>
2020-01-09 22:03:15 +0000

    Aligning with master/

--- rres/dockerhub-deploy.sh
+++ rres/dockerhub-deploy.sh
@@ -15,10 +15,11 @@ cd ../common/quickstart
 echo -e "\n\n\tPreparing the environment\n"
 
 wall -n <<EOT
-*** WARNING: Continuous Integration Scripts are going to update Docker images automatically ***
+*** WARNING: Continuous Integration is going to update Docker images ***
 
-In few minutes, existing unused images will be DELETED. The container named wheat-ci will be restarted.
-EOT 
+In few minutes, existing unused images will be DELETED.
+The container named wheat-ci will be restarted.
+EOT
 
 echo -e "--- Stopping, cleaning and updating Docker\n"
 docker stop wheat-ci || true
@@ -29,23 +30,26 @@ docker pull knetminer/knetminer
 echo -e "--- Cleaning Knetminer dataset directory\n"
 ./cleanup-volume.sh --all "$dataset_dir"
 
+
+# Let's use the two Docker servers for two different test instances
+#
 if [[ "$(hostname)" =~ 'babvs72' ]]; then
-	# Two different test instances
 	echo -e "\n\n\t(Re)launching Docker, Cypher-based traverser\n"
 
-	docker_run_opts="--with-neo4j 
-		--neo4j-url bolt://babvs65.rothamsted.ac.uk:7688 
+	docker_run_opts="--with-neo4j --neo4j-url bolt://babvs65.rothamsted.ac.uk:7688 
 		--neo4j-user rouser --neo4j-pwd rouser"
+	# Enables the Cypher Debugger, to profile Cypher queries
+	# WARNING: this is a SERIOUS security hole and we keep it on ONLY for this internal instance 
+	export MAVEN_ARGS="-Dknetminer.backend.cypherDebugger.enabled=true"
 else
 		echo -e "\n\n\t(Re)launching Docker, state machine-based traverser\n"
 fi
 
 
-echo -e "\n\n\t(Re)launching Docker\n"
 ./docker-run.sh \
-  --dataset-id wheat --container-name wheat-ci \
+  --dataset-id wheat-directed --container-name wheat-ci \
   --dataset-dir "$dataset_dir" --host-port $host_port --container-memory 24G \
-  $docker_run_opts
+  $docker_run_opts \
   --detach
 
 # TODO: as a minimum, check it's up via wget, later run scripts that check sensible results are returned
