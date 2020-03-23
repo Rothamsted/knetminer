3e19cb9019c9bdf951ff4fe5bd3d7e78157ee79a
Ajit Singh <ajitprataps@yahoo.com>
2020-03-23 11:41:08 +0000

    add

--- common/server-base/src/main/webapp/static/js/jsp-config.js
+++ common/server-base/src/main/webapp/static/js/jsp-config.js
@@ -1,6 +1,3 @@
-/* 
- * globals var for api_url for genepage/ evidencepage JSP's.
- * Author: singha (20/03/2020)
- */
-var api_url = "${knetminer.api.url}";
-var species_name = "${knetminer.specieName}";
+/* singha: globals var for api_url for genepage/ evidencepage JSP's. */
+var api_url= "${knetminer.api.url}";
+var species_name= "${knetminer.specieName}";
