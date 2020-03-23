5a50ef94fc1f6e0dd9ecef4ae864336ea4932849
Ajit Singh <ajitprataps@yahoo.com>
2020-03-23 11:29:02 +0000

    jsp load api_url

--- common/server-base/src/main/webapp/static/js/jsp-config.js
+++ common/server-base/src/main/webapp/static/js/jsp-config.js
@@ -1,3 +1,6 @@
-// See the main POM for details.
+/* 
+ * globals var for api_url for genepage/ evidencepage JSP's.
+ * Author: singha (20/03/2020)
+ */
 var api_url = "${knetminer.api.url}";
 var species_name = "${knetminer.specieName}";
