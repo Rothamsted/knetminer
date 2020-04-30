ad50a59a19127b951c822148e66f51c784531f75
Marco Brandizi <marco.brandizi@gmail.com>
2020-04-30 16:15:35 +0100

    Very minor change to comments and Travis script.

--- travis-build.sh
+++ travis-build.sh
@@ -7,15 +7,18 @@ set -e # Fail fast upon the first error
 
 if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
 	
-	# Travis's cron doesn't consider if there have been changes or not, so...
+	# Travis's cron doesn't consider if there have been changes or not, so we rely on 
+	# git commits to check that. This isn't perfect (eg, last build could have failed due to network problems,
+	# not necessarily the code itself), but good enough in most cases. 
+	# TODO: see if the date of the last successful build can be get from the Travis API.
 	#
-	nchanges=$(git log --since 'yesterday' --format=oneline | wc -l)
+	nchanges=$(git log --since '24 hours ago' --format=oneline | wc -l)
 	if [[ $(($nchanges)) == 0  ]]; then
 		cat <<EOT
 
 
-	This is a cron-triggered build and the code didn't change since last build, so we're not rebuilding.
-	This is based on github logs (--since 'yesterday'). Please, trigger a manual build if I didn't get it right.
+	This is a cron-triggered build and the code didn't change since the latest build, so we're not rebuilding.
+	This is based on github logs (--since '24 hours ago'). Please, launch a new build manually if I didn't get it right.
 	
 EOT
 	exit
