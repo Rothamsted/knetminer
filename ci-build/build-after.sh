d0463d47a05eccdba538bb4f40fb6f963adddd14
brandizi <brandizi@localhost>
2021-01-14 16:46:18 +0000

    Migrating to GH Actions.

---
+++ ci-build/build-after.sh
@@ -0,0 +1,55 @@
+echo -e "\n\n\t Cleaning local Maven for Docker"
+mvn --quiet clean
+
+echo -e "\n\n\tBuilding Docker"
+docker build -t knetminer/knetminer:$docker_tag -f docker/Dockerfile .
+
+
+if [[ "$GIT_BRANCH" != 'master' ]]; then
+	echo -e "\n\n\tThis isn't a Docker-deployed branch, Not pushing to DockerHub\n"
+	exit
+fi
+
+
+# The bare image is usually rebuilt manually, here we've only to tag it when releasing
+#
+if [[ ! `$IS_RELEASE` ]]; then
+	echo -e "\n\n\tTagging and pushing new Docker images with ${NEW_RELEASE_VER}\n"
+	# We need it for the tag
+	docker pull knetminer/knetminer-bare
+	docker_release_tag="$NEW_RELEASE_VER"	
+else
+	echo -e "\n\n\tPushing new Docker images\n"
+fi
+
+
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+
+for postfix in bare ''
+do
+	[[ -z "$postfix" ]] || postfix="-$postfix"
+
+  # See above
+  if [[ "$postfix" != '-bare' ]]; then
+  	echo "Pushing Docker$postfix:$docker_tag"
+		docker push knetminer/knetminer$postfix:$docker_tag
+	fi
+
+  $IS_RELEASE || continue 
+
+  # When we're releasing, we further tag the same images with the release tag and
+  # push them too. This should only happen for the master branch
+
+	echo "Tagging Docker$postfix with $docker_release_tag"
+	tag_str=":$docker_release_tag"
+	docker tag knetminer/knetminer$postfix knetminer/knetminer$postfix$tag_str
+	 
+	echo "Pushing Docker$postfix$tag_str"
+	docker push knetminer/knetminer$postfix$tag_str
+	
+	echo 
+done
+
+$IS_RELEASE; || exit
+
+# git push is managed by the main build.sh script	
---
+++ ci-build/build-body.sh
@@ -0,0 +1,15 @@
+# We build on an image that already has the right environment.
+# This makes it independent on the CI system that is being used
+# 
+# Built files are still landed locally thanks to --volume. This allows for CI-based caching
+# and to make binaries available for the next steps like runtime images building.
+#
+docker run -it --rm \
+  --volume `pwd`:/root/knetminer-build/knetminer \
+  --volume ~/.m2:/root/.m2 \
+  --workdir /root/knetminer-build/knetminer \
+  --env JAVA_TOOL_OPTIONS \
+  --env KNET_REPO_USER \
+  --env KNET_REPO_PASSWORD \
+  knetminer/knetminer-bare:$docker_tag \
+  /bin/bash -c "mvn $MAVEN_GOAL --settings ci-build/maven-settings.xml --update-snapshots $MAVEN_ARGS"
