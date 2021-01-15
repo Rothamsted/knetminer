35028eed842cd9967134be2685e4e45b549a36f8
brandizi <brandizi@localhost>
2021-01-15 14:54:58 +0000

    Finalising CI files for releasing. [ci skip]

--- ci-build/build-after.sh
+++ ci-build/build-after.sh
@@ -50,11 +50,4 @@ do
 	echo 
 done
 
-
-echo -e "\n\n\tTriggering RRes deployment\n"
-
-job=`echo 'Knetminer - Deploy from DockerHub' | sed s/' '/'%20'/g`
-curl --user "$KNET_JENKINS_USER:$KNET_JENKINS_TOKEN" -X POST -o - --fail \
-     "https://knetminer.org/build/job/$job/buildWithParameters" 
-
 # git push is managed by the main build.sh script	
