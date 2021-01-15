3ee109eb24d44f106b27276e57fa3cc9c8f30712
brandizi <brandizi@localhost>
2021-01-15 01:52:58 +0000

    Testing Jenkins API.

--- ci-build/build-after.sh
+++ ci-build/build-after.sh
@@ -50,8 +50,11 @@ do
 	echo 
 done
 
+
+echo -e "\n\n\tTriggering RRes deployment\n"
+
 job='Knetminer - Deploy from DockerHub'
-curl --user "$KNET_JENKINS_USER:$KNET_JENKINS_TOKEN" -X POST -o - \
+curl --user "$KNET_JENKINS_USER:$KNET_JENKINS_TOKEN" -X POST -o - --fail \
      "https://knetminer.org/build/job/$job/buildWithParameters" 
 
 # git push is managed by the main build.sh script	
