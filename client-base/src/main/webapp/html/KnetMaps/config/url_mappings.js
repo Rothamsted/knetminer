acce7369162f5612e658ba515a2263e3ff40d2f2
Ajit Singh <ajitprataps@yahoo.com>
2021-09-28 15:18:43 +0100

    amend araPheno url

--- client-base/src/main/webapp/html/KnetMaps/config/url_mappings.js
+++ client-base/src/main/webapp/html/KnetMaps/config/url_mappings.js
@@ -3,8 +3,8 @@ var url_mappings= {
  "html_acc": [
      {"cv": "AC", "weblink": "http://pmn.plantcyc.org/ARA/NEW-IMAGE?type=NIL&object=", "cc_restriction": ""},
      {"cv": "AspGD", "weblink": "http://www.aspergillusgenome.org/cgi-bin/locus.pl?dbid=", "cc_restriction": ""},
-     {"cv": "AraPheno", "weblink" : "https://arapheno.1001genomes.org/study/", "cc_restriction": ""},
-     {"cv": "ARAPHENO", "weblink" : "https://arapheno.1001genomes.org/study/", "cc_restriction": ""},
+     {"cv": "AraPheno", "weblink" : "https://arapheno.1001genomes.org/phenotype/", "cc_restriction": ""},
+     {"cv": "ARAPHENO", "weblink" : "https://arapheno.1001genomes.org/phenotype/", "cc_restriction": ""},
      {"cv": "AraGWAS", "weblink" : "https://aragwas.1001genomes.org/#/study/", "cc_restriction": ""},
      {"cv": "ARAGWAS", "weblink" : "https://aragwas.1001genomes.org/#/study/", "cc_restriction": ""},
      {"cv": "CHEBI", "weblink": "http://www.ebi.ac.uk/chebi/searchId.do?chebiId=", "cc_restriction": ""},
