c94351494cca86165f17c48302d3d99336c9f4d3
manlikleo <olaotan104@gmail.com>
2022-02-07 18:12:24 +0000

    reset button feature

---
+++ aratiny/manual-test/run-client.sh
@@ -0,0 +1,10 @@
+cd "$(dirname $0)"
+mydir="$(pwd)"
+
+echo -e "\n\n\t------ Launching the Knetminer Test Client via Maven -------\n"
+echo -e "\tShould be at http://localhost:8080 once you see it stops for listening. Stop with Ctrl-C\n"
+
+cd ../aratiny-client
+mvn clean jetty:run-war $MAVEN_ARGS # jetty:run is not enough, there are some Maven placeholders that aren't resolved by it
+
+echo -e "\n\nService should have gone down now, but don't forget the server.\n"
