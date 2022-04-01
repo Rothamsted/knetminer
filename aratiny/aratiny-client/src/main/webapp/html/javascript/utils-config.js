a3e3b790bd2d3e227e65f0985de9319a7adda44d
Marco Brandizi <marco.brandizi@gmail.com>
2022-04-01 16:47:33 +0100

    Naming conventions and minor bugs.

--- aratiny/aratiny-client/src/main/webapp/html/javascript/utils-config.js
+++ aratiny/aratiny-client/src/main/webapp/html/javascript/utils-config.js
@@ -1,4 +1,5 @@
 // See the main POM for details.
+// TODO: switch the name format to Js conventions (uncapitalisedCamelCase)
 var api_base_url = "${knetminer.api.baseUrl}";
 var api_url = "${knetminer.api.url}";
 // boolean, tells if the data set contains reference genome info  
