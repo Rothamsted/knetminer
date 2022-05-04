4292e368f12311063192f9e09d47d59868b08840
Marco Brandizi <marco.brandizi@gmail.com>
2022-05-04 02:00:34 +0200

    Trying another fix for docker command in CI script.

--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -15,5 +15,5 @@ docker run --rm \
   --env KNET_REPO_USER \
   --env KNET_REPO_PASSWORD \
   knetminer/knetminer-bare:$docker_tag \
-  /bin/bash -c "mvn $MAVEN_GOAL --settings ci-build/maven-settings.xml --update-snapshots $MAVEN_ARGS"
+  mvn $MAVEN_GOAL --settings ci-build/maven-settings.xml --update-snapshots $MAVEN_ARGS
 
