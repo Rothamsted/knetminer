9c0570b53ae3c5d0083a0515d8a6411ec4f40190
Marco Brandizi <marco.brandizi@gmail.com>
2022-09-01 10:59:36 +0100

    Google Analytics, yet another attempt to please the damn GA API.

--- server-base/src/main/java/rres/knetminer/datasource/server/KnetminerServer.java
+++ server-base/src/main/java/rres/knetminer/datasource/server/KnetminerServer.java
@@ -467,6 +467,7 @@ public class KnetminerServer
       		.documentTitle ( pageName )
       		.documentPath ( "/" + pageName )
       		.protocolVersion ( "2" )
+      		.userAgent ( "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36" )
       )
 			.build ();
 				
