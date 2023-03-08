c3be0981383ee49ce2624d3062effb8d6d76df31
Keywan Hassani-Pak <keywan.hassani-pak@rothamsted.ac.uk>
2023-03-08 06:51:09 +0000

    Removed unused startup files

--- rres/babvs80/knetminer-ascomycota-public.sh
+++ rres/babvs80/knetminer-ascomycota-public.sh
@@ -2,14 +2,14 @@ set -e
 mydir=`dirname "$0"`
 mydir=`realpath "$mydir"`
 
-echo -e "\n\n\tRestarting https://knetminer.com/TODO/\n"
+echo -e "\n\n\tRestarting https://knetminer.com/ascomycota/\n"
 
 . "$mydir/../_utils.sh"
 
 cd "$HOME/software/knetminer/docker"
 
-container_id=poaceae-test 
-dataset_dir="$HOME/data/poaceae-54"
+container_id=ascomycota
+dataset_dir="$HOME/data/ascomycota-public"
 clean_flag="$1"
 
 prepare_docker "$container_id" "$dataset_dir" "$clean_flag"
@@ -17,5 +17,4 @@ prepare_docker "$container_id" "$dataset_dir" "$clean_flag"
 export DOCKER_OPTS="-it --restart unless-stopped"
 # TODO: latest is unstable
 ./docker-run.sh --dataset-dir "$dataset_dir" --image-version latest \
-  --container-name "$container_id" --host-port 8088 --container-memory 40G --detach
-
+  --container-name "$container_id" --host-port 8083 --container-memory 40G --detach
