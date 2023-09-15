25c01602e91bc036e7ca833bda476f9b81fd5822
Olaotan Lawal <olaotan104@gmail.com>
2023-09-15 11:42:01 +0100

    #789 Fixes evidenceTable infinitescrolling bug.

--- client-base/src/main/webapp/html/javascript/evidence-table.js
+++ client-base/src/main/webapp/html/javascript/evidence-table.js
@@ -197,10 +197,6 @@ function createEvidenceTable( tableData, doSortTable=false )
     evidenceTableScroller.setupScrollHandler ()
 }
 
-
-
-
-
 /*
  * Function
  * Generates multi evidence network in KnetMaps
@@ -416,10 +412,10 @@ function createEvidenceTableBody ( tableData, doAppend = false )
 	 * 
 	 * if it's a bug, fix it with a loop over the right window 
 	 */
-  
-  tableData.forEach( (evidence,index) => 
-  {   
-    let {conceptType, name, pvalue, totalGenesSize, ondexId, userGenesSize, userGeneAccessions } = evidence
+
+    for(var row = fromRow; row < toRow; row++){
+ 
+    let {conceptType, name, pvalue, totalGenesSize, ondexId, userGenesSize, userGeneAccessions } = tableData[row]
 
     // Prefer this templating style, at least for new code
     // Also, avoid "x = x + ...", it's more verbose than +=, especially when
@@ -427,8 +423,8 @@ function createEvidenceTableBody ( tableData, doAppend = false )
     //  
     tableBody +=`<tr>
                     <td>
-                    <p onclick="evidenceTableExcludeKeyword(${ondexId},this,event)" id="evidence_exclude_${index}" style="padding-right:10px;" class="excludeKeyword evidenceTableExcludeKeyword" title="Exclude term"></p>
-                        <p onclick="evidenceTableAddKeyword(${ondexId},this,event)" id="evidence_add_${index}" class="addKeyword evidenceTableAddKeyword" title="Add term"></p>
+                    <p onclick="evidenceTableExcludeKeyword(${ondexId},this,event)" id="evidence_exclude_${row}" style="padding-right:10px;" class="excludeKeyword evidenceTableExcludeKeyword" title="Exclude term"></p>
+                        <p onclick="evidenceTableAddKeyword(${ondexId},this,event)" id="evidence_add_${row}" class="addKeyword evidenceTableAddKeyword" title="Add term"></p>
                     </td>`;
 
     //link publications with pubmed
@@ -459,11 +455,13 @@ function createEvidenceTableBody ( tableData, doAppend = false )
     tableBody += `  <td ><span style="margin-right:.5rem;">${totalGenesSize}</span> <span data-type="${conceptType}" data-description="${name}" class="accession-download" onclick="openGeneListPopup(${ondexId},this)"><i class="fas fa-file-download"></i></span> <div id="concept${ondexId}"></div></td>\n`;
 
     // launch evidence network with them, if they're not too many.
-    tableBody += `  <td><button data-genelist="${userGeneAccessions}" style="${userGenesSize == 0 ? 'text-decoration:none;': null}" onclick="openAccessionNetworkView(${ondexId},event)"  class="userGenes_evidenceNetwork" title="Display in KnetMaps" id="userGenes_evidenceNetwork_${index}">${userGenesSize}</button></td>\n`;
+    tableBody += `  <td><button data-genelist="${userGeneAccessions}" style="${userGenesSize == 0 ? 'text-decoration:none;': null}" onclick="openAccessionNetworkView(${ondexId},event)"  class="userGenes_evidenceNetwork" title="Display in KnetMaps" id="userGenes_evidenceNetwork_${row}">${userGenesSize}</button></td>\n`;
 
-    var select_evidence = `<input onchange="updateSelectedGenesCount('evidences', '#evidence-count', 'Term');" id="checkboxEvidence_${index}" type="checkbox" name= "evidences" value="${ondexId}:${userGeneAccessions}">`;
+    var select_evidence = `<input onchange="updateSelectedGenesCount('evidences', '#evidence-count', 'Term');" id="checkboxEvidence_${row}" type="checkbox" name= "evidences" value="${ondexId}:${userGeneAccessions}">`;
     tableBody += `  <td>${select_evidence}</td>\n`; // eviView select checkbox
-  })
+
+    }
+  
   
   if ( tableBody )
   {
