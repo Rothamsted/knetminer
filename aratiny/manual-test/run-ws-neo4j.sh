a3dbcb1cee7c36dc08d85674ca32800c0e29f88a
Marco Brandizi <marco.brandizi@gmail.com>
2023-09-15 16:28:30 +0100

    --no-clean option added to run-ws.sh

--- aratiny/manual-test/run-ws-neo4j.sh
+++ aratiny/manual-test/run-ws-neo4j.sh
@@ -1,4 +1,4 @@
 cd "$(dirname $0)"
 
 #export MAVEN_ARGS="$MAVEN_ARGS -Dneo4j.server.version=4.3.1"
-./run-ws.sh neo4j
+./run-ws.sh neo4j $*
--- aratiny/manual-test/run-ws.sh
+++ aratiny/manual-test/run-ws.sh
@@ -2,9 +2,13 @@ set -e
 cd "$(dirname $0)"
 mydir="$(pwd)"
 
+# Syntax: [ <profile>[,<profile>]* [-nc|--no-clean] ]
+
 profiles="$1"
+no_clean="$2"
+
 export MAVEN_OPTS="$MAVEN_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
-# Suppresses pesky warnnings from Jetty. BE CAREFUL. They might be significant
+# Suppresses pesky warnings from Jetty. BE CAREFUL. They might be significant
 export MAVEN_OPTS="$MAVEN_OPTS -Dorg.slf4j.simpleLogger.log.org.eclipse.jetty.annotations.AnnotationParser=ERROR"
 
 echo -e "\n\n\t------ Launching the Knetminer Test Server via Maven -------\n"
@@ -15,6 +19,9 @@ cd ../aratiny-ws
 #
 [ "$profiles" == "" ] || profiles="$profiles,"
 profiles="${profiles}console"
-mvn $MAVEN_ARGS -P"$profiles" clean verify
+
+( [[ "$no_clean" == "-nc" ]] || [[ "$no_clean" == "--no-clean" ]] ) && mvn_clean='' || mvn_clean='clean'
+
+mvn $MAVEN_ARGS -P"$profiles" $mvn_clean verify
 
 echo -e "\n\nService should have gone down now, but don't forget the client.\n"
