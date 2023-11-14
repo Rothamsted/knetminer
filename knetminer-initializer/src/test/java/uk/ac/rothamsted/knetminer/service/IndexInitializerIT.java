20593194f66ab9a7bd182b811188319caac236db
Vitaly Vyurkov <VitalyV@i.ua>
2023-11-14 09:56:13 +0200

    Enhancement of IndexInitializerIT class

--- knetminer-initializer/src/test/java/uk/ac/rothamsted/knetminer/service/IndexInitializerIT.java
+++ knetminer-initializer/src/test/java/uk/ac/rothamsted/knetminer/service/IndexInitializerIT.java
@@ -43,13 +43,68 @@ public class IndexInitializerIT {
     }
 
     @Test
-    public void testIndexCreation () {
+    public void testIndexBuildingFromPropertiesSet () {
         var neoDriver = neoDriverResource.getDriver ();
         var indexInitializer = new IndexInitializer();
         indexInitializer.setDatabase ( neoDriver );
 
         indexInitializer.createConceptsIndex(Set.of ( "prefName", "altName", "Phenotype" ));
 
+        testIndexBuilding ();
+/*
+        try ( Session session = neoDriver.session() )
+        {
+            String cypherQuery = """
+				SHOW ALL INDEXES WHERE name = "concept_index"
+				""";
+            Result result = session.run( cypherQuery );
+            var propertiesList = result.next().get("properties").asList();
+            log.info("Returned properties list: {}", propertiesList);
+            var basePropertiesList = List.of("Phenotype", "Phenotype_1", "altName", "prefName");
+            assertEquals("Returned properties list has gotten to be equal to the base properties list" ,propertiesList.toString(), basePropertiesList.toString());
+        }
+*/
+    }
+
+    @Test
+    public void testReadFromConfig ()
+    {
+        var knetInitializer = KnetMinerInitializerTest.createKnetMinerInitializer ( false, true );
+        var indInit = new IndexInitializer ();
+        indInit.setDatabase ( neoDriverResource.getDriver () );
+
+        indInit.createConceptsIndex ( knetInitializer );
+
+        testIndexBuilding ();
+    }
+
+    @Test
+    public void testReadAllFromConfig ()
+    {
+        var knetInitializer = KnetMinerInitializerTest.createKnetMinerInitializer ( false, true );
+        var indInit = new IndexInitializer ();
+        indInit.setDatabase ( knetInitializer );
+
+        indInit.createConceptsIndex ( knetInitializer );
+
+        testIndexBuilding ();
+    }
+
+    @Test
+    public void testReadAllFromConfigViaBoltURL ()
+    {
+        var knetInitializer = KnetMinerInitializerTest.createKnetMinerInitializer ( false, true );
+        var indInit = new IndexInitializer ();
+        indInit.setDatabase ( "config://", null, null, knetInitializer );
+
+        indInit.createConceptsIndex ( knetInitializer );
+
+        testIndexBuilding ();
+    }
+
+    private void testIndexBuilding ()
+    {
+        var neoDriver = neoDriverResource.getDriver ();
         try ( Session session = neoDriver.session() )
         {
             String cypherQuery = """
